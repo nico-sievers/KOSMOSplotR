cf6d7af6e270146da6af8db273a8e9f081ce05d3
Sievers <nisievers@geomar.de>
2024-03-10 18:52:15 +0100

    tests fix 1

--- tests/testthat/testplotfunctions.R
+++ tests/testthat/testplotfunctions.R
@@ -2,4 +2,7 @@ context("testplotfunctions")  # Our file is called "test-check_output.R"
 library(testthat)             # load testthat package
 library(KOSMOSplotsR)         # load our package
 
+
+KOSMOStestdata=da
+
 KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord")

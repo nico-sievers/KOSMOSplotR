cb28eca8ffac909a3dd6f8f845b649633683bdf9
Sievers <nisievers@geomar.de>
2024-03-10 18:44:20 +0100

    added test package

---
+++ R/KOSMOStestdata.R
@@ -0,0 +1,18 @@
+#' Artificial dataset to showcase the functions within the KOSMOSplotsR package
+#'
+#' @docType data
+#'
+#' @usage data(KOSMOStestdata)
+#'
+#' @format An object of class \code{"data.frame"} as if loaded from a standard KOSMOS excel data sheet
+#' \describe{
+#'  \item{Parameter}{Random, unit-less values for a response variable}
+#' }
+#' @references This data set was artificially created by Nico Sievers
+#' @keywords dataset
+#' @examples
+#'
+#' data(KOSMOStestdata)
+#' head(KOSMOStestdata)
+#'
+"KOSMOStestdata"
---
+++ tests/testthat/testplotfunctions.R
@@ -0,0 +1,5 @@
+context("testplotfunctions")  # Our file is called "test-check_output.R"
+library(testthat)             # load testthat package
+library(KOSMOSplotsR)         # load our package
+
+KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord")

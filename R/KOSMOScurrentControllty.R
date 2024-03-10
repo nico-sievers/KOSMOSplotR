c7c73b3d18b153d8f12e92f75bfca0511344e94a
Sievers <nisievers@geomar.de>
2024-03-10 23:27:30 +0100

    definition update datasets

---
+++ R/KOSMOScurrentControllty.R
@@ -0,0 +1,18 @@
+#' Definition of the linetype used for drawing the control
+#'
+#' @docType data
+#
+# @usage data(KOSMOStestdata)
+#
+# @format An object of class \code{"data.frame"} as if loaded from a standard KOSMOS excel data sheet
+# \describe{
+#  \item{Parameter}{Random, unit-less values for a response variable}
+# }
+# @references This data set was artificially created by Nico Sievers
+# @keywords dataset
+# @examples
+#
+# data(KOSMOStestdata)
+# head(KOSMOStestdata)
+#
+"KOSMOScurrentControllty"

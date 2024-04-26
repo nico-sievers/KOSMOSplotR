5e7af0b13050b9ab9762faf9527233b86c7f7b5c
Sievers <nisievers@geomar.de>
2024-04-26 13:35:19 +0200

    fixed test

--- tests/testthat/testplotfunctions.R
+++ tests/testthat/testplotfunctions.R
@@ -3,12 +3,12 @@ library(KOSMOSplotR)
 
 KOSMOStimeplot()
 KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord")
-KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord",ignore = 1)
-KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord",ylabel = "Parameter [fantasy unit]",xlabel = "Timeline - Experiment day")
+KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord",exclude_meso=1)
+KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord",ylabel="Parameter [fantasy unit]",xlabel="Timeline - Experiment day")
 #KOSMOStimeplot(KOSMOStestdata,"Parameter",control="Fjord",stats.show = T)
 
 
 KOSMOSregplot()
-KOSMOSregplot(day = c(3,4,5))
-KOSMOSregplot(ignore = 5)
+KOSMOSregplot(day=c(3,4,5))
+KOSMOSregplot(ignore=5)
 KOSMOSregplot(daylabellocation = "bottom")

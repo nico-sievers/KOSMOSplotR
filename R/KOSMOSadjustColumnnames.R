d30c1a0817275d22017cd57c09cf5de480b2a759
Nico Sievers <nico.sievers@gmx.net>
2025-02-09 00:59:24 +0100

    push of the new column handling functions

--- R/KOSMOSadjustColumnnames.R
+++ R/KOSMOSadjustColumnnames.R
@@ -4,24 +4,57 @@
 #'
 #' @param dataset A data set object roughly following the common KOSMOS layout for which the column names shall be adjusted to fit the template.
 #' @param required_columns Vector of those columns to be matched, all others are left untouched. Defaults to all potentially required columns for the available plot functions. This argument is supplied by the plotting function if this function is called automatically, so that only strictly necessary ones are marked as required.
+#' @param minimal_requirement Vector of those columns that essential for plotting KOSMOS data and can't be derived from others.
 #'
 #' @return The same data set with (potentially) modified column names.
 #'
 # @examples
 # KOSMOSadjustColumnnames(KOSMOStestdata)
-#'
 #' @export
+#' @importFrom dplyr select
 
-KOSMOSadjustColumnnames=function(dataset,required_columns=c("Day","Mesocosm",KOSMOScurrentCategoricalVar,KOSMOScurrentContinuousVar,"Treat_Meso")){
 
-  tmp=KOSMOSguessColumnnames(dataset,required_columns)
-  if(tmp[[1]]!="perfect"){
-    if(length(tmp[[2]])==nrow(tmp[[3]])){
-      names(dataset)[tmp[[2]]]=tmp[[3]]$Names
+KOSMOSadjustColumnnames=function(dataset,required_columns=c("Day","Mesocosm",KOSMOScurrentCategoricalVar,KOSMOScurrentContinuousVar,"Treat_Meso"),minimal_requirement=c("Day","Mesocosm")){
+
+  # run the function that does all of the pattern searches, then work based on its output
+  tmp=KOSMOSguessColumnnames(dataset,required_columns,minimal_requirement)
+
+  if(tmp[[1]]=="perfect"){
+    message("All essential columns were found by names, let's go ahead")
+  } else {
+    # if it wasn't a perfect fit, jump into fixing action
+
+    # is there anything to rename?
+    if((length(tmp[[3]])+length(tmp[[4]]))>0){
+      names(dataset)[names(dataset) %in% tmp[[4]]]=tmp[[3]]
+
       message("They were interpreted as:")
-      message(paste(tmp[[3]]$Names,collapse=" | "))
+      message(paste(tmp[[3]],collapse=" | "))
+      message("Please check whether this assumption is correct!")
     }
+
+    if(tmp[[1]]=="need addition"){ # if some are missing, they will now be added in based on the style sheet
+
+      # fuck meso wird vorher renamed, also ist dieses ganze mitschleppen des orig namens bullshit
+      styletmp=KOSMOScurrentStyletable[c("Mesocosm",tmp[[6]])]
+      names(styletmp)[1]="tmp_meso"
+      styletmp$tmp_meso=as.character(styletmp$tmp_meso)
+      dataset$tmp_meso=#[,tmp[[5]]]=
+        as.character(gsub("m","",dataset[[tmp[[5]]]]))
+      dataset=merge(styletmp,dataset,all.y=T,by="tmp_meso")
+      #dataset=dataset[,-c("tmp_meso_clean")]
+      #whereisday=which(names(dataset)=="Day")
+      dataset=dataset %>%
+        select(Day,Mesocosm,everything()) %>%
+        select(-tmp_meso)
+
+      message("Not all essential column were found by name, so they were created from the style template and added to your data set:")
+      message(paste(tmp[[6]],collapse=" | "))
+      message("Please check the treatment assigment!")
+
+    }
+    message("These are the updated column names:")
+    print(names(dataset))
   }
   return(dataset)
-
 }
--- R/KOSMOSguessColumnnames.R
+++ R/KOSMOSguessColumnnames.R
@@ -4,6 +4,7 @@
 #'
 #' @param dataset A data set object roughly following the common KOSMOS layout for which the column names shall be checked.
 #' @param required_columns Vector of those columns to be matched, all others are left untouched. Defaults to all potentially required columns for the available plot functions. This argument is supplied by the plotting function if this function is called automatically, so that only strictly necessary ones are marked as required.
+#' @param minimal_requirement Vector of those columns that essential for plotting KOSMOS data and can't be derived from others.
 #'
 #' @return Returns an info value on how well the found columns matched the original, and the column numbers of the matches, and a boiled down version of the \code{KOSMOScolumntable} for internal use by \code{KOSMOSadjustColumnnames}.
 #'
@@ -14,37 +15,88 @@
 #' @importFrom utils head tail
 
 KOSMOSguessColumnnames=function(dataset=KOSMOStestdata,
-                               required_columns=c("Day","Mesocosm",KOSMOScurrentCategoricalVar,KOSMOScurrentContinuousVar,"Treat_Meso")){
+                               required_columns=c("Day","Mesocosm",KOSMOScurrentCategoricalVar,KOSMOScurrentContinuousVar,"Treat_Meso"),
+                               minimal_requirement=c("Day","Mesocosm")){
 
+  # set export variables as empty
+  fit=NA
+  guessed_columns=character(0)
+  guessed_columns_positions=NA
+  #create_columns=character(0)
+  essentials_position=character(0)
+  meso_position=character(0)
+  missing_cols_guessed=character(0)
+  # prepare error message
+  failed="Couldn't identify all essential columns in the data set!\nRequiring at least an equivalent of 'Day' and 'Mesocosm'."
+
+  # get the info on expected columns from the input and the table of campaign-specific names
   tmpcolumntable=KOSMOScolumntable[KOSMOScolumntable$Names %in% required_columns,]
-  # this searches for exact matches ### THIS DOES ALWAYS PRINT, SO SOMETHING DOESN'T WORK
-  # tmp=unlist(lapply(tmpcolumntable$Names,grep,names(dataset),fixed=T))
-  tmp=which(names(dataset) %in% tmpcolumntable$Names)
-  if(length(tmp)==nrow(tmpcolumntable)){
-    fit="perfect"
-  } else {
-    # this searches for patterns now
-    tmp=lapply(tmpcolumntable$Patterns,grep,names(dataset),ignore.case=T,perl=T)
-    nottmp=lapply(tmpcolumntable$NotMatch,grep,names(dataset),ignore.case=T,perl=T)
-    tmp=Map(setdiff,tmp,nottmp)
-    # here i always use the first match of each, assuming that later occurances are something different
-    tmp=unique(unlist(lapply(tmp,head,1)))
 
+  # which of those don't exact matches in the data set?
+  missing_cols_perfect=setdiff(tmpcolumntable$Names,names(dataset))
+  perfect_matches_columns=setdiff(required_columns,missing_cols_perfect) # report these later
+  if(length(missing_cols_perfect) == 0){
+    fit="perfect" # if all are there exactly as expected
 
-    message("The following columns were identified:")
-    message(paste(names(dataset)[tmp],collapse=" | "))
-    fit="suggested"
-    if(length(tmp)!=nrow(tmpcolumntable)){
-      stop("Couldn't identify all essential columns in the data set!\nRequiring an equivalent of:\n",paste(tmpcolumntable$Names,collapse=" | "))
+  } else { # no perfect match, but still hope
+
+    # this searches for patterns now to find those with small naming differences
+    yestmp=lapply(tmpcolumntable$Patterns,grep,names(dataset),ignore.case=T,perl=T) # this is the match pattern
+    nottmp=lapply(tmpcolumntable$NotMatch,grep,names(dataset),ignore.case=T,perl=T) # this is the patters to exclude
+    tmp=Map(setdiff,yestmp,nottmp) # they ore brought together for the final decision which columns match and which dont
+    guessed_columns_ideal_names_position=unlist(lapply(tmp,function(x) length(x)>0))
+    guessed_columns_positions=unique(unlist(lapply(tmp,head,1))) # here i always use the first match of each, assuming that later occurrences are something different. also i exclude duplicates, so if two patterns were matched on the same column name thats also excluded here
+
+    guessed_columns_current_names=setdiff(names(dataset)[guessed_columns_positions],required_columns)
+    guessed_columns_ideal_names_full=tmpcolumntable$Names[guessed_columns_ideal_names_position]
+    missing_cols_guessed=setdiff(tmpcolumntable$Names,guessed_columns_ideal_names_full) # which ones were not sucessfully guessed?
+    guessed_columns_ideal_names=setdiff(guessed_columns_ideal_names_full,perfect_matches_columns) # remove those that were also a perfect match anyway. report these later
+
+    if(length(guessed_columns_ideal_names_full) >= 2){ # only continue if you matched at least two
+
+      create_columns=setdiff(required_columns,guessed_columns_ideal_names_full) # report these later
+
+      if(length(guessed_columns_ideal_names)>0){
+        # if you guessed some that are not anyway perfect fits
+      message("When scanning for the required columns, the following ones were identified to have slighlty different Names:")
+      message(paste(guessed_columns_current_names,collapse=" | "))
+      }
+
+      if(length(missing_cols_guessed) == 0){
+        fit="guessed all" # if all were found by the pattern search
+
+      } else if(all(!(minimal_requirement %in% missing_cols_guessed))){
+        # if at least Day and Mesocosm are there. the rest can be calculated
+
+        # this repeats the pattern search for only meso
+        tmpkeycolumntable=KOSMOScolumntable[KOSMOScolumntable$Names %in% minimal_requirement,] # get the info on expected columns from the input and the table of campaign-specific names
+        # # day first
+        # yestmpday=grep(tmpkeycolumntable$Patterns[1],names(dataset),ignore.case=T,perl=T) # this is the match pattern
+        # nottmpday=grep(tmpkeycolumntable$NotMatch[1],names(dataset),ignore.case=T,perl=T) # this is the patters to exclude
+        # tmpday=setdiff(yestmpday,nottmpday) # they are brought together for the final decision which columns match and which dont
+        # meso next
+        yestmpmeso=grep(tmpkeycolumntable$Patterns[2],names(dataset),ignore.case=T,perl=T) # this is the match pattern
+        nottmpmeso=grep(tmpkeycolumntable$NotMatch[2],names(dataset),ignore.case=T,perl=T) # this is the patters to exclude
+        meso_position=first(setdiff(yestmpmeso,nottmpmeso)) # they ore brought together for the final decision which column matches and which doesnt
+
+        fit="need addition"
+        #essentials_position=c(tmpday,tmpmeso) # pass on the key columns
+
+      } else {
+        # this triggers if there are guessed matches, but Day and Meso are not among them
+        stop(failed)
+      }
+    } else {
+      # this triggers if we have less than two guessed matches
+      stop(failed)
     }
   }
-  return(list(fit,tmp,tmpcolumntable))
-
+  return(list(fit,perfect_matches_columns,guessed_columns_ideal_names,guessed_columns_current_names,meso_position,missing_cols_guessed))
 }
 
 
 
-
+if(T){
 # # Sample data
 # entries <- c("mesoabc", "treatmeso", "mesotreat", "treatmesoabc", "xyzmesotreat", "abc")
 #
@@ -98,3 +150,4 @@ KOSMOSguessColumnnames=function(dataset=KOSMOStestdata,
 #
 # # Output the result
 # print(result)
+}
